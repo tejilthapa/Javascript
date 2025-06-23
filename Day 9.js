4185537d9a751a0673f23e0e3d71700f6580d9c4
tejilthapa <tejilthapa09@gmail,com>
2025-06-23 16:50:46 +0530

    new

---
+++ Day 9.js
@@ -0,0 +1,20 @@
+// for of loop
+
+let ar=['tejil',45,'bhopal',5.67]
+console.log(ar[3])
+
+for(let v of ar)
+{
+    console.log(v)
+}
+
+// push()
+
+ar.push("India","MP")
+
+console.log(ar)
+
+//pop()
+
+ar.pop()
+console.log(ar)
