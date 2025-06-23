17cbff7c7f97c35136cd380caace86786a4ddd87
tejilthapa <tejilthapa09@gmail,com>
2025-06-23 17:27:24 +0530

    New

--- Day 9.js
+++ Day 9.js
@@ -1,20 +1,66 @@
-// for of loop
+// // for of loop
 
-let ar=['tejil',45,'bhopal',5.67]
-console.log(ar[3])
+// let ar=['tejil',45,'bhopal',5.67]
+// console.log(ar[3])
 
-for(let v of ar)
+// for(let v of ar)
+// {
+//     console.log(v)
+// }
+
+// // push()
+
+// ar.push("India","MP")
+
+// console.log(ar)
+
+// //pop()
+
+// ar.pop()
+// console.log(ar)
+
+// //unshift()
+
+// ar.unshift("101", "MP")
+// console.log(ar)
+
+// //shift
+
+// ar.shift()
+// console.log(ar)
+
+// Slice
+
+// let ar1=["php",45,10.7,"MP"]
+// let ar2=ar1.slice(1,3)
+// console.log(ar2)
+
+// // Splice
+
+// ar.splice(2,0,'Sir')
+// console.log(ar)
+
+
+let ar=["Cy",2,3,"N"]
+ar1=ar.slice(2,3)
+for(let i=1; i<11; i++)
 {
-    console.log(v)
+    if(ar1>0)
+    {
+        console.log(ar1*i)
+    }
+    else{
+        console.log("IV")
+    }
 }
 
-// push()
 
-ar.push("India","MP")
+let arr=[1,3,5,2,4,10,11,15]
 
-console.log(ar)
+arr1=arr.slice(0,1)
+console.log(arr1)
 
-//pop()
-
-ar.pop()
-console.log(ar)
+var a=parseInt(prompt("From where to start:"))
+var b=parseInt(prompt("How many to delete:"))
+arr.splice(a,b)
+console.log(arr)
