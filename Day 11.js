3e8a291241a6549b4fb97504338591837b43051e
tejilthapa <tejilthapa09@gmail,com>
2025-06-25 17:12:10 +0530

    New

---
+++ Day 11.js
@@ -0,0 +1,46 @@
+
+
+// function add()
+// {
+//     console.log("inside the function")
+// }
+
+// add()
+// add()
+// add()
+// add() //code reusibility
+
+
+// function sum()
+// {
+//     let a =10
+//     let b =20
+//     console.log(a+b)
+// }
+ 
+// sum()
+
+// function sub()
+// {
+//     let a = parseInt(prompt("Enter a 1st number: "))
+//     let b = parseInt(prompt("Enter the 2nd number"))
+//     console.log(a-b)
+// }
+
+// sub()
+
+// wap to print the table of any number using any function
+
+// function tab()
+// {
+//     let a=parseInt(prompt("Enter the number"))
+
+//     for (let i =1; i<=10; i++)
+//     {
+//         console.log(a*i)
+//     }
+// }
+
+// tab()
+
+
