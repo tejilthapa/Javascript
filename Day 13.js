193c42831e878c54957c850f23c7b4c606b06894
tejilthapa <tejilthapa09@gmail,com>
2025-06-27 17:12:41 +0530

    Update

---
+++ Day 13.js
@@ -0,0 +1,12 @@
+let a = document.getElementById('head1')
+console.log(a)
+
+// Text change
+
+a.textContent="inside js"
+
+let b = document.getElementById('head2')
+b.innerHTML="<i>Clouds</i>"
+
+let c = document.getElementsByClassName('head3')
+c[0].textContent="change with js"
