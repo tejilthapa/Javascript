fd345e91fb88e08a5f6275821d246e503cbcebf5
tejilthapa <tejilthapa09@gmail,com>
2025-06-27 16:45:50 +0530

    Update

--- Day 12.js
+++ Day 12.js
@@ -2,3 +2,20 @@
 
 let fun = () => {return console.log("Hi")}
 fun()
+
+
+
+let ob ={
+     
+    name:"Tejil",
+    age:21,
+    contact:6846842,
+    address:"Bhopal",
+    location:function(){
+        alert{"MP Nagar"}
+    },
+    country : () => {return "India"}
+}
+
+ob.location()
+console.log(ob.country())
