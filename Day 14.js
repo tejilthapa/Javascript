bd00a740f41380353d8f966da151f3e5c2e0e911
tejilthapa <tejilthapa09@gmail,com>
2025-06-28 17:15:44 +0530

    Update

--- Day 14.js
+++ Day 14.js
@@ -1,7 +1,25 @@
-let h1= document.getElementById("head1")
+// let h1= document.getElementById("head1")
 
-h1.style.backgroundColor="red"
-h1.style.color="white"
+// h1.style.backgroundColor="red"
+// h1.style.color="white"
 
-let image = document.getElementById('image')
-image.src="fakename.jpeg"
+// let image = document.getElementById('image')
+// image.src="fakename.jpeg"
+
+
+let h1=document.getElementById('head1')
+
+h1.style.backgroundColor="yellow"
+h1.style.color="red"
+
+let h2=document.getElementById('h2')
+
+h2.style.backgroundColor="blue"
+h2.style.color="pink"
+
+let image=document.getElementById('image')
+
+image.src="f2.jpeg"
+image.style.borderRadius="50%"
+image.style.height="200px"
+image.style.width="200px"
