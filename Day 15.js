ecff3b4dad9c9f19df2b849380d4b63b5e93009d
tejilthapa <tejilthapa09@gmail,com>
2025-06-30 17:20:13 +0530

    Update

--- Day 15.js
+++ Day 15.js
@@ -1,24 +1,41 @@
-function fun()
-{
-    alert("working...!!!")
+// function fun()
+// {
+//     alert("working...!!!")
+// }
+
+
+// function changetext()
+// {
+//     let head = document.getElementById('head')
+//     head.textContent = "M.P Nagar"
+// }
+
+
+// function ct()
+// {
+//     let h1 = document.getElementById('h1')
+//     h1.textContent = "Get Lost"
+// }
+
+// function ct1()
+// {
+//     let h1 = document.getElementById('h1')
+//     h1.textContent = "Hello"
+// }
+
+
+function cc1() {
+    let h1 = document.getElementById('h1');
+    h1.textContent = "Red";
+    document.body.style.backgroundColor = "red";
 }
-
-
-function changetext()
-{
-    let head = document.getElementById('head')
-    head.textContent = "M.P Nagar"
+function cc2() {
+    let h1 = document.getElementById('h1');
+    h1.textContent = "Green";
+    document.body.style.backgroundColor = "green";
 }
-
-
-function ct()
-{
-    let h1 = document.getElementById('h1')
-    h1.textContent = "Get Lost"
-}
-
-function ct1()
-{
-    let h1 = document.getElementById('h1')
-    h1.textContent = "Hello"
+function cc3() {
+    let h1 = document.getElementById('h1');
+    h1.textContent = "Yellow";
+    document.body.style.backgroundColor = "yellow";
 }
