f90647e333a1af052d4a25f26d4c7d18e8d43656
tejilthapa <tejilthapa09@gmail,com>
2025-06-30 17:05:33 +0530

    New

---
+++ Day 15.js
@@ -0,0 +1,24 @@
+function fun()
+{
+    alert("working...!!!")
+}
+
+
+function changetext()
+{
+    let head = document.getElementById('head')
+    head.textContent = "M.P Nagar"
+}
+
+
+function ct()
+{
+    let h1 = document.getElementById('h1')
+    h1.textContent = "Get Lost"
+}
+
+function ct1()
+{
+    let h1 = document.getElementById('h1')
+    h1.textContent = "Hello"
+}
