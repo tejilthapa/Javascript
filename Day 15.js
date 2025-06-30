dc20dda7daa6d3f13bf2f7a9dedf91b0d9d28778
tejilthapa <tejilthapa09@gmail.com>
2025-06-30 17:30:10 +0530

    Add files via upload

---
+++ Day 15.js
@@ -0,0 +1,31 @@
+// function fun()
+// {
+//     alert("working...!!!")
+// }
+
+
+// function changetext()
+// {
+//     let head = document.getElementById('head')
+//     head.textContent = "M.P Nagar"
+// }
+
+
+// function ct()
+// {
+//     let h1 = document.getElementById('h1')
+//     h1.textContent = "Get Lost"
+// }
+
+// function ct1()
+// {
+//     let h1 = document.getElementById('h1')
+//     h1.textContent = "Hello"
+// }
+
+
+function cc(a) {
+    let h1 = document.getElementById('h1');
+    h1.textContent = a;
+    document.body.style.backgroundColor = a;
+}
