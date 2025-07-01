4a5e356748b70c3da6538d6fca911bb63f5f867f
tejilthapa <tejilthapa09@gmail.com>
2025-07-01 17:19:31 +0530

    Add files via upload

---
+++ Day 16.js
@@ -0,0 +1,20 @@
+// function change1()
+// {
+//     let a = document.getElementById('changeimg')
+//     changeimg.src="f2.jpeg"
+// }
+// function change2()
+// {
+//     let a = document.getElementById('changeimg')
+//     a.src="flower.jpeg"
+// }
+
+
+let h = document.querySelector('h1')
+h.style.color = "red"
+
+let head = document.querySelector('.head')
+head.style.color = "blue"
+
+let h1 = document.querySelector('#h2')
+h1.style.color = "green"
