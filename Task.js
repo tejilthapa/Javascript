e8006d74efb488c913f92b78bc89ccbe57b0035a
tejilthapa <tejilthapa09@gmail,com>
2025-07-03 16:43:23 +0530

    Update

--- Task.js
+++ Task.js
@@ -6,7 +6,7 @@ function fun()
     let d = document.querySelector('#gen').value
     let e = document.querySelector('#rel').value
 
-    document.querySelector('#show1').textContent = ${a}\n ${b}\n ${c}\n ${d}\n ${e}\n
+    document.querySelector('#show1').textContent =`${a}\n ${b}\n ${c}\n ${d}\n ${e}\n`
 
     return false
 }
