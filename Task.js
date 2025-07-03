e24c64e843178ecca9f31f29438da901da6c24f3
tejilthapa <tejilthapa09@gmail,com>
2025-07-03 16:40:14 +0530

    Udpate

---
+++ Task.js
@@ -0,0 +1,12 @@
+function fun()
+{
+    let a = document.querySelector('#name').value
+    let b = document.querySelector('#email').value
+    let c = document.querySelector('#dob').value
+    let d = document.querySelector('#gen').value
+    let e = document.querySelector('#rel').value
+
+    document.querySelector('#show1').textContent = ${a}\n ${b}\n ${c}\n ${d}\n ${e}\n
+
+    return false
+}
