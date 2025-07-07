8343fe2ecf79e8b0a8504d77e7b7f817d9b3ad05
tejilthapa <tejilthapa09@gmail,com>
2025-07-07 17:12:12 +0530

    Update

--- Day 19.js
+++ Day 19.js
@@ -5,4 +5,20 @@ let emp =[
     {name:"Mukesh",age:21,city:"Raipur",contact:1774144},
     {name:"Saurabh",age:21,city:"Saagar",contact:5515558}
 ]
-console.log(emp)
+// console.log(emp)
+
+// let newob=[{name:"Aakash",age:23,city:"Delhi",contact:733561}]
+// emp.unshift(newob)
+// console.log(emp)
+
+// let val1 = emp.slice(2,3)
+
+// console.log(val1)
+
+
+
+
+
+
+
+
