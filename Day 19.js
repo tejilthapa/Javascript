a20f7ebdce1890d360c3baa0bed1b1359eaccc92
tejilthapa <tejilthapa09@gmail.com>
2025-07-07 16:54:26 +0530

    Add files via upload

---
+++ Day 19.js
@@ -0,0 +1,8 @@
+let emp =[
+    {name:"Tejil",age:21,city:"Indore",contact:4654634},
+    {name:"Adil",age:22,city:"Bhopal",contact:1212121},
+    {name:"Nitin",age:20,city:"Gwalior",contact:9989899},
+    {name:"Mukesh",age:21,city:"Raipur",contact:1774144},
+    {name:"Saurabh",age:21,city:"Saagar",contact:5515558}
+]
+console.log(emp)
