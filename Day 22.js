53fe0ef0f445fa336d211095079c5212655d0837
tejilthapa <tejilthapa09@gmail.com>
2025-07-11 17:12:27 +0530

    Add files via upload

---
+++ Day 22.js
@@ -0,0 +1,17 @@
+// let ob = {
+//     name = "ankit",
+//     contact : 165413564,
+//     city : "bhopal" 
+// }
+
+// localStorage.getItem()
+
+
+function validate()
+{
+    let userdata = {
+        name:document.querySelector('#name').value,
+        contact:document.querySelector('#con').value,
+        
+    }
+}
