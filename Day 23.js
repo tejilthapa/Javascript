5e32e86c1c30a74e564b78548eddebf21d881a05
tejilthapa <tejilthapa09@gmail,com>
2025-07-12 17:07:01 +0530

    Update

--- Day 23.js
+++ Day 23.js
@@ -1,14 +1,30 @@
-//Destructuring of array 
+// //Destructuring of array 
 
-let [name,num,city] = ["Cybrom",75,"Bhopal"]
+// let [name,num,city] = ["Cybrom",75,"Bhopal"]
 
-console.log(name)
+// console.log(name)
 
-// spread Operator
+// // spread Operator
 
-let ar1 = ["google", "azure"]
-let ar2 = ["amazon", "flip"]
-let ar3 = [...ar1, ...ar2,"ar3"]
-ar3 = [...ar3]
+// let ar1 = ["google", "azure"]
+// let ar2 = ["amazon", "flip"]
+// let ar3 = [...ar1, ...ar2,"ar3"]
+// ar3 = [...ar3]
 
-console.log(ar3)
+// console.log(ar3)
+
+// Object
+
+// let {name,contact,age} = {
+//     name:"Vivek",
+//     contact:0212432,
+//     age:50
+// }
+
+// console.log(name)
+
+let ob = {n:1,m:2}
+let ob1 = {n1:1,m2:2}
+let ob2 = {...ob, ...ob1}
+
+console.log(ob2)
