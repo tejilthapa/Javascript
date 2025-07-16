4aa6d58d286a9d5ece6dd6f4365f77eb3aa98697
tejilthapa <tejilthapa09@gmail.com>
2025-07-16 17:18:35 +0530

    Add files via upload

---
+++ Day 24.js
@@ -0,0 +1,14 @@
+console.log("This is the main js")
+
+// import module1 from "./Day 24 module1.js";
+
+// console.log(module1(10,20))
+
+import demo from "./Day 24 module1.js";
+
+console.log(demo(9,1))
+
+import {add, sub} from './Day 24 module2.js'
+
+add()
+sub()
