dc6fef62f7d197cf0d5129a7db41f3f4e60abaca
tejilthapa <tejilthapa09@gmail,com>
2025-07-16 17:18:55 +0530

    Update

---
+++ Day 24 module1.js
@@ -0,0 +1,6 @@
+function module1(a,b){
+    return a+b
+}
+
+
+export default module1
---
+++ Day 24 module2.js
@@ -0,0 +1,9 @@
+function add(){
+    console.log('This is the add function')
+}
+
+function sub(){
+    console.log('This is the sub function')
+}
+
+export {add, sub}
