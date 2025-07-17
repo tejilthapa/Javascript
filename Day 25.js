720f00ada590f0384b68f928eb579f4a0a81b362
tejilthapa <tejilthapa09@gmail,com>
2025-07-17 17:24:04 +0530

    Update

--- Day 25.js
+++ Day 25.js
@@ -4,8 +4,21 @@
 async function demo (){
     let respone = await fetch("https://jsonplaceholder.typicode.com/commentshttps://jsonplaceholder.typicode.com/comments")
     let data = await respone.json()
+    // let a = data.map((e)=>{return e.name})
 
-    console.log(data);
+    let apidata = data.map((e)=>`
+    <tr>
+        <td>${e.id}</td>
+        <td>${e.name}</td>
+        <td>${e.email}</td>
+    </tr>
+    `).join(" ")
+
+    let savedata = document.querySelector("#savedata")
+    savedata.innerHTML = apidata
+
+    // console.log(data);
+    // console.log(a)
 }
 
 demo()
